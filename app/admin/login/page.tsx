45140130a2ab194647883865a07284198df1e36e
PIYUSH-PC\Piyush Gupta <pkgupta.pe.ug@jadavpuruniversity.in>
2024-03-29 16:09:03 +0530

    add: AddTeam card and Login

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
-function page() {
-  return <div>Re-directed to login if not logged in</div>;
+const Login = () => {
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Authorized personnel Only!
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className="grid gap-2">
+        <label className="block text-sm font-medium text-gray-700">
+                  ID
+                </label>
+                  <input
+                    id="name"
+                    name="name"
+                    type="name"
+                    autoComplete="name"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-200 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                  />
+        </div>
+        <div className="grid gap-2">
+        <label className="block text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="password"
+                    required
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-200 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                  />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button className="w-full">Sign in</Button>
+      </CardFooter>
+    </Card>
+  )
 }
+export default Login
 
-export default page;
+//TODO:Re-directed to login if not logged in
