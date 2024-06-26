a87ce36704cba3fb800b7623fa11d8fba27d1de4
Prannay Kedia <prannaykedia1@gmail.com>
2024-06-26 09:49:31 +0530

    commented out emailing system

--- lib/emailer.ts
+++ lib/emailer.ts
@@ -7,34 +7,34 @@ export default async function sendMail(
   sub: string,
   textBody: string
 ) {
-  try {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: fromMail, // sender address
-      replyTo: fromMail, // Sender's Email
-      to: toMail, // list of receivers
-      subject: sub, // Subject line
-      text: textBody, // plain text body
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    return info.messageId;
-  } catch (error) {
-    console.log(error);
-    return new Error(String(error));
-  }
+  // TODO: Change it after update
+  return "No email now";
+  // try {
+  //   // create reusable transporter object using the default SMTP transport
+  //   let transporter = nodemailer.createTransport({
+  //     service: "gmail",
+  //     host: "smtp.gmail.com",
+  //     port: 465,
+  //     secure: true,
+  //     auth: {
+  //       user: process.env.SMTP_USER,
+  //       pass: process.env.SMTP_PASSWORD,
+  //     },
+  //   });
+  //   // send mail with defined transport object
+  //   let info = await transporter.sendMail({
+  //     from: fromMail, // sender address
+  //     replyTo: fromMail, // Sender's Email
+  //     to: toMail, // list of receivers
+  //     subject: sub, // Subject line
+  //     text: textBody, // plain text body
+  //   });
+  //   console.log("Message sent: %s", info.messageId);
+  //   return info.messageId;
+  // } catch (error) {
+  //   console.log(error);
+  //   return new Error(String(error));
+  // }
 }
 
 export async function sendHtmlMail(
