2e1261344bead7c7d70b7f5cb338416c6c6ab69f
Prannay Kedia <prannaykedia1@gmail.com>
2024-07-30 01:22:16 +0530

    JU Footer added

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "@/components/admin/Sidebar";
 import LoadingState from "./loading";
 import { Suspense } from "react";
+import Link from "next/link";
 
 export default function Layout({
   children,
@@ -10,7 +11,14 @@ export default function Layout({
   return (
     <Suspense fallback={<LoadingState />}>
       <Sidebar>
-        <div className="my-2">{children}</div>
+        <div className="flex flex-col min-h-screen">
+          <div className="my-2 flex-grow">{children}</div>
+          <footer className="text-xs text-gray-400 py-3 text-center">
+            Developed by Jadavpur University (Institution's Innovation Council).
+            Developer -{" "}
+            <Link href="www.linkedin.com/in/prannay-kedia">Prannay Kedia</Link>
+          </footer>
+        </div>
       </Sidebar>
     </Suspense>
   );
