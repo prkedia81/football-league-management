65d44caad7118738c94b899c3de291bc1e2ded36
Prannay Kedia <prannaykedia1@gmail.com>
2024-07-30 01:24:58 +0530

    Minor Build Error fixed

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -14,8 +14,8 @@ export default function Layout({
         <div className="flex flex-col min-h-screen">
           <div className="my-2 flex-grow">{children}</div>
           <footer className="text-xs text-gray-400 py-3 text-center">
-            Developed by Jadavpur University (Institution's Innovation Council).
-            Developer -{" "}
+            Developed by Jadavpur University ({"Institution's"} Innovation
+            Council). Developer -{" "}
             <Link href="www.linkedin.com/in/prannay-kedia">Prannay Kedia</Link>
           </footer>
         </div>
